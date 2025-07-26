46950f025d6b45ecac80c17f63e20b5b3d245ca1
Alexander Tyschenko <qwitelite@gmail.com>
2025-07-26 21:11:13 +0300

    created 3 implementations for MachineProducer

---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,13 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+    @Override
+    public List<Excavator> get() {
+        return List.of(new Excavator(), new Excavator(), new Excavator());
+
+   }
+}
