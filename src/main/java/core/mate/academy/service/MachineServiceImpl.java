1f3be00ccf944c036515487d55dbf78a8e13fd1a
Alexander Tyschenko <qwitelite@gmail.com>
2025-07-26 22:07:59 +0300

    implemented interface MachineService and class MachineServiceImpl

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,45 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+
+    @Override
+    public List<T> getAll(Class<? extends T> type) {
+        MachineProducer machineProducer = null;
+        if (type == Bulldozer.class) {
+           machineProducer = new BulldozerProducer();
+        } else if (type == Truck.class) {
+            machineProducer = new TruckProducer();
+        } else if (type == Excavator.class) {
+            machineProducer = new ExcavatorProducer();
+        } else {
+            return List.of();
+        }
+        List<T> machines = machineProducer.get();
+        return new ArrayList<>(machines);
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends T> machines) {
+        for (T machine: machines) {
+            machine.doWork();
+        }
+    }
 }
